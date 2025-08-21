5efb9f37d08a5310b32e45d8c75cb76735bd409c
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-08-21 11:34:32 -0500

    Add files via upload

--- SQLQueryPropio.sql
+++ SQLQueryPropio.sql
@@ -1,7 +1,23 @@
-CREATE TABLE clientes(
-ID INT primary key,
-Nombre NVARCHAR(100),
-Apellido NVARCHAR(100),
-Email NVARCHAR(100),
-fechaRegistro DATE
+CREATE TABLE Clientees (
+    id_Cli INT PRIMARY KEY,
+    Nombre VARCHAR(100),
+    Correo VARCHAR(100)
 );
+
+CREATE TABLE Pedidos (
+    id_Pedidos INT PRIMARY KEY,
+    id_Cli INT,
+    Producto VARCHAR(100),
+    Fecha DATE,
+    FOREIGN KEY (id_Cli) REFERENCES Clientees(id_Cli)
+);
+
+INSERT INTO Clientees (id_Cli, Nombre, Correo)
+VALUES (1, 'Victor', 'ejemplo@gmail.com');
+
+INSERT INTO Pedidos (id_Pedidos, id_Cli, Producto, Fecha)
+VALUES (101, 1, 'Celular Xiaomi note 14', '2025-08-21');
+
+INSERT INTO Pedidos (id_Pedidos, id_Cli, Producto, Fecha)
+VALUES (102, 1, 'Laptop Lenovo', '2025-08-21');
+
