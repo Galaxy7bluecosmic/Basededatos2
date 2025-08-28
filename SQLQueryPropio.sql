11a4fa48700a6c4a2be548ad863da446af9060e9
Galaxy7bluecosmic <Vizcardocanovictorpaul@gmail.com>
2025-08-28 11:13:52 -0500

    semana 3 practica

--- SQLQueryPropio.sql
+++ SQLQueryPropio.sql
@@ -23,5 +23,24 @@ VALUES
 (101, 1, 'Celular Xiaomi note 14', '2025-08-21'),
 (102, 1, 'Laptop Lenovo', '2025-08-21'),
 (103,2, 'monitor redragon','2025-08-21'),
-(103,3, 'monitor hp','2025-08-21');
+(104,3, 'monitor hp','2025-08-21');
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME='Pedidos';
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME='Clientees';
+
+Select Nombre, correo from Clientees;
+
+Select * from Clientees Order By Nombre ASC;
+
+select 
+C.Nombre,
+C.Correo,
+P.Producto,
+P.Fecha
+FROM Clientees C
+INNER JOIN Pedidos P On C.id_Cli= P.id_Cli
+ORDER BY C.Nombre ASC;
 
